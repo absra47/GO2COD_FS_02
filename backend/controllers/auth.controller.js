0dee734f20d3356d27cbebf598bb9b126ae3d11c
absra47 <absragirma630@gmail.com>
2024-11-12 18:12:58 +0300

    database setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = async (req, res) => {
+  res.send("signup router called");
+};
+export const login = async (req, res) => {
+  res.send("login router called");
+};
+export const logout = async (req, res) => {
+  res.send("logout router called");
+};
