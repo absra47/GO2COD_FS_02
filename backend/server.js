aabaf311b520ff12898e2525d68d7ed5f7dc1891
absra47 <absragirma630@gmail.com>
2024-11-16 09:36:31 +0300

    Cart Routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
 import { connectDB } from "./lib/db.js";
 dotenv.config();
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json()); //allow you to parse the body of the request
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 app.listen(PORT, () => {
   console.log("Server is lising on port http://localhost:" + PORT);
   connectDB();
