39722c2f974009656c23dbce5406dc7d233af400
absra47 <absragirma630@gmail.com>
2024-11-18 22:44:06 +0300

    Analytics Routes

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
 import { connectDB } from "./lib/db.js";
 import paymentRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
 dotenv.config();
 const app = express();
 
@@ -18,6 +19,7 @@ app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
 app.listen(PORT, () => {
   console.log("Server is lising on port http://localhost:" + PORT);
   connectDB();
