302b955c101745929a346c01466621c60440c751
absra47 <absragirma630@gmail.com>
2024-11-18 19:05:20 +0300

    Payment Routes with Stripe

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
 import { connectDB } from "./lib/db.js";
+import paymentRoutes from "./routes/payment.route.js";
 dotenv.config();
 const app = express();
 
@@ -16,6 +17,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
 app.listen(PORT, () => {
   console.log("Server is lising on port http://localhost:" + PORT);
   connectDB();
